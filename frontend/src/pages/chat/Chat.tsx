f7440ceef1ff5ce1ed1aa9eebab754f15f6a3c80
msi-martinsen <147509815+msi-martinsen@users.noreply.github.com>
2023-10-10 14:15:41 +0200

    Update Chat.tsx
    Updated image ref

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -550,7 +550,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Azure}
+                                    src={martinsen}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
